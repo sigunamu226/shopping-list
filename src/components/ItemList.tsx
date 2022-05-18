7e7aa7c1368b2b17d3a42386c1be646ad49ac2d1
sigunamu226 <sigunamu226@gmail.com>
2022-05-18 16:50:10 +0900

    二つのリストを大枠で囲むように
    リストをスクロールできるように

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -1,25 +1,31 @@
+import DeleteIcon from "@mui/icons-material/Delete";
 import {
+  Box,
   Checkbox,
   IconButton,
-  List,
   ListItem,
   ListItemButton,
   ListItemIcon,
   ListItemText,
 } from "@mui/material";
 import React, { useEffect, useState } from "react";
-import DeleteIcon from "@mui/icons-material/Delete";
 import { Col } from "react-bootstrap";
+import { FixedSizeList, ListChildComponentProps } from "react-window";
 import { Item } from "../interfaces/item";
+import "./itemlist.scss";
 
 const ItemList: React.FC<{
   item: Item[];
   setItem: React.Dispatch<React.SetStateAction<Item[]>>;
 }> = ({ item, setItem }) => {
   const [checked, setChecked] = useState<boolean[]>([]);
+  const [boxHeight, setBoxHeight] = useState(0);
+  const [boxWidth, setBoxWidth] = useState(0);
 
   useEffect(() => {
     setChecked([...checked, false]);
+    setBoxHeight(document.getElementById("item-list-box")!.clientHeight);
+    setBoxWidth(document.getElementById("item-list-box")!.clientWidth);
   }, [item]);
 
   const onCheck = (key: number) => {
@@ -27,45 +33,56 @@ const ItemList: React.FC<{
   };
 
   return (
-    <Col className="item-list-">
-      <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
-        {item.map((item, i) => {
-          const labelId = `checkbox-list-label-${i}`;
-
-          return (
-            <ListItem
-              key={i}
-              secondaryAction={
-                <IconButton edge="end" aria-label="comments">
-                  <DeleteIcon />
-                </IconButton>
-              }
-              disablePadding
-            >
-              <ListItemButton
-                role={undefined}
-                dense
-                onClick={() => {
-                  onCheck(i);
-                }}
-              >
-                <ListItemIcon>
-                  <Checkbox
-                    edge="start"
-                    tabIndex={-1}
-                    disableRipple
-                    checked={checked[i]}
-                    inputProps={{ "aria-labelledby": labelId }}
-                  />
-                </ListItemIcon>
-                <ListItemText id={labelId} primary={item.name} />
-              </ListItemButton>
-            </ListItem>
-          );
-        })}
-      </List>
+    <Col className="item-list">
+      <Box className="item-list-box" id="item-list-box">
+        <FixedSizeList
+          height={boxHeight}
+          width={boxWidth}
+          itemSize={46}
+          itemCount={item.length}
+          overscanCount={5}
+        >
+          {renderRow}
+        </FixedSizeList>
+      </Box>
     </Col>
   );
+
+  function renderRow(props: ListChildComponentProps) {
+    const { index, style } = props;
+
+    return (
+      <ListItem
+        key={index}
+        secondaryAction={
+          <IconButton edge="end" aria-label="comments">
+            <DeleteIcon />
+          </IconButton>
+        }
+        style={style}
+        className="list-item"
+      >
+        <ListItemButton
+          role={undefined}
+          dense
+          onClick={() => {
+            onCheck(index);
+          }}
+        >
+          <ListItemIcon>
+            <Checkbox
+              edge="start"
+              tabIndex={-1}
+              disableRipple
+              checked={checked[index]}
+              inputProps={{ "aria-labelledby": "aaa" }}
+            />
+          </ListItemIcon>
+          <ListItemText id="aaa" primary={item[index].name} />
+        </ListItemButton>
+      </ListItem>
+    );
+  }
 };
 
 export default ItemList;
