ba9b41615134035f825fbaa885b27cdf218f9e5e
InoueHayato226 <inouehayato@rootteam.co.jp>
2022-08-09 02:03:22 +0900

    アップデート

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ItemBoard from "./components/ItemBoard";
+import SignUp from "./components/SignUp";
+import { AuthProvider } from "./context/AuthContext";
 
 const App: React.FC = () => {
   return (
-    <>
-      <ItemBoard />
-    </>
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="itemboard" element={<ItemBoard />} />
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
   );
 };
 
