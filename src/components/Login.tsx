e6f26f224bcac59e8923766e0f3a3b9aa0a694be
sigunamu226 <sigunamu226@gmail.com>
2022-08-09 23:07:33 +0900

    ログイン画面作成

---
+++ src/components/Login.tsx
@@ -0,0 +1,106 @@
+import {
+  Avatar,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  Paper,
+  Stack,
+  TextField,
+} from "@mui/material";
+import React, { useState } from "react";
+import { Container, Form } from "react-bootstrap";
+import Image from "../asset/image/bandicam-2021-02-02-21-19-47-932.jpg";
+import {
+  initError,
+  initLogin,
+  onCheckValidation,
+  serviceOnChangeInput,
+} from "../services/login";
+import { auth } from "../firebase.js";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import "./signup.scss";
+import { IError, ILogin, InputType } from "../interfaces/login";
+
+const Login: React.FC = () => {
+  const [loginState, setLoginState] = useState<ILogin>(initLogin());
+  const [errorState, setErrorState] = useState<IError>(initError());
+
+  const onChangeInput = (
+    type: InputType,
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    serviceOnChangeInput(type, e, loginState, setLoginState);
+  };
+
+  const onClickLogin = async () => {
+    if (onCheckValidation(loginState, setErrorState)) {
+      return false;
+    }
+
+    await signInWithEmailAndPassword(
+      auth,
+      loginState.email,
+      loginState.password
+    );
+  };
+
+  return (
+    <Container className="signup-container">
+      <Paper className="signup-paper">
+        <Avatar alt="icon" src={Image} className="icon" />
+        <h3>ようこそ買い物メモへ</h3>
+        <Form className="input-form">
+          <Stack spacing={3}>
+            <TextField
+              fullWidth
+              required
+              error={errorState.email}
+              helperText={errorState.email && errorState.emailErrorMessage}
+              type="email"
+              id="outlined-basic"
+              label="メールアドレス"
+              variant="outlined"
+              value={loginState?.email}
+              onChange={(e) => {
+                onChangeInput(InputType.EMAIL, e);
+              }}
+            />
+            <TextField
+              fullWidth
+              required
+              error={errorState.password}
+              helperText={
+                errorState.password && errorState.passwordErrorMessage
+              }
+              id="outlined-basic"
+              label="パスワード"
+              type="password"
+              variant="outlined"
+              value={loginState?.password}
+              onChange={(e) => {
+                onChangeInput(InputType.PASS, e);
+              }}
+            />
+            <FormControlLabel
+              control={<Checkbox />}
+              label="ログイン状態を保存する"
+            />
+          </Stack>
+          <Button
+            className="mt-3"
+            variant="contained"
+            fullWidth
+            onClick={onClickLogin}
+          >
+            ログイン
+          </Button>
+          <Button className="mt-2" variant="text" fullWidth>
+            パスワードを忘れた場合
+          </Button>
+        </Form>
+      </Paper>
+    </Container>
+  );
+};
+
+export default Login;
