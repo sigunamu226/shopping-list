921ece3f8d8e7afb8fac87c3036b38d74710906c
sigunamu226 <sigunamu226@gmail.com>
2022-08-10 00:40:07 +0900

    ログアウトの実装
    ログアウト後はログイン画面に遷移するように

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,15 @@
 import { AppBar, Button, Toolbar, Typography } from "@mui/material";
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../firebase.js";
 
 const Header: React.FC = () => {
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    auth.signOut();
+    navigate("/login");
+  };
+
   return (
     <div>
       <AppBar position="static">
@@ -9,7 +17,9 @@ const Header: React.FC = () => {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             買う物メモ
           </Typography>
-          <Button color="inherit">ログアウト</Button>
+          <Button color="inherit" onClick={handleLogout}>
+            ログアウト
+          </Button>
         </Toolbar>
       </AppBar>
     </div>
