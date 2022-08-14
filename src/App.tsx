cf5b78e8c0aaa5d86ae5312d7b94aff981b6cbb2
sigunamu226 <sigunamu226@gmail.com>
2022-08-14 16:43:41 +0900

    /にアクセスした時、ログイン画面にリダイレクトするようにz

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import ItemBoard from "./components/ItemBoard";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
@@ -13,6 +13,9 @@ const App: React.FC = () => {
           <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
           <Route path="/itemboard" element={<ItemBoard />} />
+          <Route path="/" element={<Navigate to="/login" replace />} />
+          {/* replaceを付けると、指定されたURLページに置き換えるのと、
+ブラウザの戻るボタンを使って以前のページに戻ることができないようにする。 */}
         </Routes>
       </AuthProvider>
     </BrowserRouter>
